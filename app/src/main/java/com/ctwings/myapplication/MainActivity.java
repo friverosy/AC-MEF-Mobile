ea3c9a6f7ea5b9ca7701117f93eec175469a96ce
Cristtopher Quintana T <cquintana@patagoniawings.com>
2016-09-12 12:22:39 -0300

    change Toast LENGTH_LONG by LENGTH_SHORT

--- app/src/main/java/com/ctwings/myapplication/MainActivity.java
+++ app/src/main/java/com/ctwings/myapplication/MainActivity.java
@@ -822,6 +822,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void makeToast(String msg){
-        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
 }
